e8d9073cb4b6f396ee92b0076a08c79e7f85ac26
JACK <jkear@seas.upenn.edu>
2017-08-09 12:09:38 -0400

    added controller file for login

---
+++ public/javascript/login.js
@@ -0,0 +1,45 @@
+angular.module("loginApp", [])
+	.controller('loginCtrl', function ($scope, $state, $http) {
+		$scope.loginUsername = "";
+		$scope.loginPassword = "";
+
+		console.log('controller loginCtrl is running...');
+		
+		$scope.login = function () {
+			var obj = {
+				username: $scope.loginUsername, 
+				password: $scope.loginPassword
+			}
+			$http({
+				method: 'POST', 
+				url: 'login', 
+				data: JSON.stringify(obj)
+			}).then(function onSuccess(response) {
+				if (response.data)
+					$scope.errorMsg = response.data;
+			}).catch(function onError(errResponse) {
+				if (errResponse.data)
+					$scope.errorMsg = errResponse.data;
+			});
+		};
+
+		$scope.register = function () {
+			var obj = {
+				username: $scope.regUsername, 
+				password: $scope.regPassword, 
+				name: $scope.regName
+			}
+			$http({
+				method: 'POST', 
+				url: 'login', 
+				data: JSON.stringify(obj)
+			}).then(function onSuccess(response) {
+				if (response.data)
+					$scope.errorMsg = response.data;
+			}).catch(function onError(errResponse) {
+				if (errResponse.data)
+					$scope.errorMsg = errResponse.data;
+			});
+		};
+		
+	});
