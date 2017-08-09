90510c286bf1a8a2067e05b075b6744965590a7f
shady459 <shady.mimari@gmail.com>
2017-08-09 11:40:42 -0400

    api

--- routes/getMyAccount.js
+++ routes/getMyAccount.js
@@ -4,8 +4,11 @@ var bodyParser = require('body-parser')
 var models = require('./models.marketplaceModel')
 
 router.post('/getmyAccount', function(req, res, next) {
-	
- 
+	models.find({ username: '' }, function(err, myaccount) {
+ 	  if (err) throw err;
+	  // object of the user
+	  console.log(myaccount);
+	});
 });
 
 module.exports = router;
