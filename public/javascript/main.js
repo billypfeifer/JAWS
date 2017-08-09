ab4a714ef790e730fad8ea250305943a949b1f87
JACK <jkear@seas.upenn.edu>
2017-08-09 10:35:52 -0400

    push rest of app to branch

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,19 +1,19 @@
-angular.module("myApp", ['ui.router'])
-	.config(function($stateProvider, $urlRouterProvider) {
-		$urlRouterProvider.otherwise('/');
-		var marketplace = {
-			name: 'market', 
-			url: '/marketplace',
-			templateUrl: 'Home.html'
-		};
-		var profile = {
-			name: 'profile', 
-			url: 'profile', 
-			templateUrl: 'profile.html'
-		}
-		$stateProvider.state(profile);
-		$stateProvider.state(marketplace);
-	})
+angular.module("myApp")
+	// .config(function($stateProvider, $urlRouterProvider) {
+	// 	$urlRouterProvider.otherwise('/');
+	// 	var marketplace = {
+	// 		name: 'market', 
+	// 		url: '/marketplace',
+	// 		templateUrl: 'Home.html'
+	// 	};
+	// 	var profile = {
+	// 		name: 'profile', 
+	// 		url: 'profile', 
+	// 		templateUrl: 'profile.html'
+	// 	}
+	// 	$stateProvider.state(profile);
+	// 	$stateProvider.state(marketplace);
+	// })
 	.controller('myCtrl', function ($scope) {
 		console.log('controller myCtrl is running...');
 		$scope.hello = "OMG HELLO";
