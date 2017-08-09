ff3dc47cb32ac0d53079ec79f939aa210e65e046
JACK <jkear@seas.upenn.edu>
2017-08-09 10:17:28 -0400

    added public files

---
+++ public/javascript/main.js
@@ -0,0 +1,20 @@
+angular.module("myApp", ['ui.router'])
+	.config(function($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise('/');
+		var marketplace = {
+			name: 'market', 
+			url: '/marketplace',
+			templateUrl: 'Home.html'
+		};
+		var profile = {
+			name: 'profile', 
+			url: 'profile', 
+			templateUrl: 'profile.html'
+		}
+		$stateProvider.state(profile);
+		$stateProvider.state(marketplace);
+	})
+	.controller('myCtrl', function ($scope) {
+		console.log('controller myCtrl is running...');
+		$scope.hello = "OMG HELLO";
+	})
